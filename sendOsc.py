55a203b90fb2699072957407deea5212a93d3813
Vincent Berthiaume <vberthiaume@gmail.com>
2025-05-23 11:00:18 -0400

    working

--- sendOsc.py
+++ sendOsc.py
@@ -1,14 +1,15 @@
 from pythonosc import udp_client
-from math import sin
+from math import sin, pi
 from time import sleep
 
 address = "127.0.0.1"
-port = 9001
+port = 9002
 client = udp_client.SimpleUDPClient(address, port)
-counter = 0
 
 while True:
-      counter+=0.1
-      for i in range(20):
-            client.send_message(f"/{i}/size", sin(i+counter)+2)
-      sleep(2)
+    for i in range(20):
+        x = (i / 19) * 2 * pi
+        value = sin(x)
+        print(value)
+        client.send_message(f"/juce/rotaryknob", value)
+        sleep(1)
